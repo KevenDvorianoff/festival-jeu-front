4269b406bbe282452ef88cec6feea9fc18262a8b
Keven <keven.dvorianoff@etu.umontpellier.fr>
2021-03-25 00:43:37 +0100

    add css for selected item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list'
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
